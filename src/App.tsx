2374c50810a5d5449aba9337d79eaa2896745276
hedysom <yevgeniy.terentyev@gmail.com>
2026-09-17 16:59:50 +0200

    header separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Button } from "./components/Button"
+import { Header } from "./components/Header"
+import { HabitForm } from "./components/HabitForm"
 
 export default function App(){
   return  (
@@ -7,25 +8,4 @@ export default function App(){
     <HabitForm/>
   </div>
   )
-}
-
-function Header(){
-  return <header className="flex items-center justify-between">
-    <div className="flex flex-col gap-1">
-      <h1 className="text-3xl font-bold">Habit Tracker</h1>
-      <span className="text-zinc-400 text-sm">1 / 1 done today</span>
-    </div>
-
-    <div className="flex flex-col gap-1">
-      <span className="text-zinc-400 text-sm">Sep 17 - Sep 18</span>
-      <div className="flex items-center gap-3">
-        <Button>Prev</Button>
-        <Button>Next</Button>
-      </div>
-    </div>
-  </header>
-}
-
-function HabitForm(){
-  return null
 }
