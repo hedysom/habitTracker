2fef83adfde3e8203407aa058d3fc1ed276eb98e
hedysom <yevgeniy.terentyev@gmail.com>
2026-09-19 22:05:51 +0200

    finished the Habits list with styling, added variants to the Button and state tu the form

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { Button } from "./Button";
 
 export function HabitForm() {
+  const [name, setName] = useState("");
   return (
     <form className="flex gap-2">
       <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2
             focus-visible:ring-orange-500"
         placeholder="New habit..."
       />
-      <Button>Add Habit</Button>
+      <Button className="rounded-lg px-4 py-2 font-medium">Add Habit</Button>
     </form>
   );
 }
--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -1,11 +1,14 @@
 import { Button } from "./Button";
+import {
+  eachDayOfInterval,
+  endOfWeek,
+  format,
+  isFuture,
+  startOfWeek,
+} from "date-fns";
 
 export default function HabitsList() {
-  const habits = [
-    { id: "fnas,d", name: "wiw" },
-    { id: "jflkrt", name: "wow" },
-    { id: "ksagfj", name: "wuw" },
-  ];
+  const habits = [{ id: "fnas,d", name: "wiw" }];
   if (habits.length === 0) {
     return (
       <p className="text-center text-zinc-500 py-12">
@@ -27,11 +30,31 @@ type HabitItemsProps = {
 };
 
 function HabitItem({ habit }: HabitItemsProps) {
+  const visibleDates = eachDayOfInterval({
+    start: startOfWeek(new Date(), { weekStartsOn: 1 }),
+    end: endOfWeek(new Date(), { weekStartsOn: 1 }),
+  });
+
   return (
-    <div className="rounded-x1 bg-zinc-800 p-4 flex flex-col gap-3">
-      <div className="flex items-center justify-between">
-        <span className="font-medium">{habit.name}</span>
-        <span className="text-sm text-amber-400"> Streak 3 </span>
+    <div className="rounded-xl bg-zinc-800 p-4 flex flex-col gap-3">
+      <div className="flex items-center justify-between ">
+        <div className="flex gap-3 items-center">
+          <span className="font-medium">{habit.name}</span>
+          <span className="text-sm text-amber-400"> Streak x Days</span>
+        </div>
+        <Button variant="ghost-destructive" className="text-sm">Delete</Button>
+      </div>
+      <div className="flex gap-1.5">
+        {visibleDates.map((date) => (
+          <Button
+            className="flex flex-1 flex-col items-center gap-0.5 rounded-lg text-xs"
+            key={date.toISOString()}
+            disabled={isFuture(date)}
+          >
+            <span className="font-medium">{format(date, "EEE")}</span>
+            <span>{format(date, "d")}</span>
+          </Button>
+        ))}
       </div>
     </div>
   );
